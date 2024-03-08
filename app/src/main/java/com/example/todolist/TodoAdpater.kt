75ba763a3b9aec23920764e0006d377798c47208
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-08 23:29:09 +0100

    update itemTodo and TodoAdpater

--- app/src/main/java/com/example/todolist/TodoAdpater.kt
+++ app/src/main/java/com/example/todolist/TodoAdpater.kt
@@ -1,24 +1,50 @@
 package com.example.todolist
 
+import android.graphics.Paint
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdpater(  private val todos: MutableList<Todo>):RecyclerView.Adapter<TodoAdpater.TodoViewHolder>() {
 
     class TodoViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val tvTodoTitle: TextView
+        val cdDone: CheckBox
 
+        init {
+            tvTodoTitle = itemView.findViewById(R.id.tvTodoTitle)
+            cdDone = itemView.findViewById(R.id.cdDone)
+        }
+    }
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean){
+        if (isChecked){
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        }else{
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        TODO("Not yet implemented")
+        return TodoViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_todo,parent,false))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return todos.size
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val curTodo = todos[position]
+        holder.tvTodoTitle.text = curTodo.title
+        holder.cdDone.isChecked = curTodo.isChecked
+        toggleStrikeThrough(holder.tvTodoTitle,curTodo.isChecked)
+        holder.cdDone.setOnCheckedChangeListener { _, isChecked ->
+            toggleStrikeThrough( holder.tvTodoTitle,isChecked)
+            holder.cdDone.isChecked = !curTodo.isChecked
+        }
     }
 }
