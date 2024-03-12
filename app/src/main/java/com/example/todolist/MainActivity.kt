c8bf608ab43778cccaac3a31a33f083033f7782d
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-12 20:53:02 +0100

    upadate in mainAcivity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.todolist
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.R.id.tvTodoTitle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdpater: TodoAdpater
+    lateinit  var myRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdpater = TodoAdpater(mutableListOf())
+        myRecyclerView.adapter = todoAdpater
     }
 }
