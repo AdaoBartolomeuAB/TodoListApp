6403a8f2633e7c944ee318f809d01aa75fb7363e
Adão Gaspar Bartolomeu <adaobartolomeua.b96@hotmail.com>
2024-03-13 19:25:42 +0100

    add button logic btnAddTodo in mainAcivity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -3,18 +3,36 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.R.id.btnAddTodo
+import com.example.todolist.R.id.etTodoTitle
 import com.example.todolist.R.id.tvTodoTitle
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var todoAdpater: TodoAdpater
+    lateinit var todoAdpater: TodoAdpater
     lateinit  var rvTodoItems: RecyclerView
+    lateinit var btnAddTodo: Button
+    lateinit var etTodoTitle: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         todoAdpater = TodoAdpater(mutableListOf())
         rvTodoItems.adapter = todoAdpater
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+        btnAddTodo = findViewById(R.id.btnAddTodo)
+        etTodoTitle = findViewById(R.id.etTodoTitle)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if (todoTitle.isNotEmpty()){
+                val todo = Todo(todoTitle);
+                todoAdpater.addTodo(todo)
+            }
+        }
     }
 }
